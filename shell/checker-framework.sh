900f4bf86ecfbe00af9abec62aa9a812d37d3a7b
Shubham Raj <31668258+the1derer@users.noreply.github.com>
2020-11-30 13:45:25 +0530

    [zsh] Add zprofile

--- shell/checker-framework.sh
+++ shell/checker-framework.sh
@@ -1,6 +1,6 @@
-# Alias for Checker Framework
-export CHECKERFRAMEWORK=/mnt/Work/Checker_Framework/build/PreBuilt/ # Use PreBuilt version of Checker Framework
-# export CHECKERFRAMEWORK=/mnt/Work/Checker_Framework/build/typetools/checker-framework # Use Typetoolsversion of Checker Framework
-# export CHECKERFRAMEWORK=/mnt/Work/Checker_Framework/build/the1derer/checker-framework # Use the1dererversion of Checker Framework
+# Set `alias` for Checker Framework
+# export CHECKERFRAMEWORK=$HOME/Work/Open_Source/checker_framework/build/prebuilt # Use PreBuilt version of Checker Framework
+export CHECKERFRAMEWORK=$HOME/Work/Open_Source/checker_framework/build/typetools/checker-framework # Use Typetool's version of Checker Framework
+# export CHECKERFRAMEWORK=$HOME/Work/Open_Source/checker_framework/build/the1derer/checker-framework # Use the1derer version of Checker Framework
 # export PATH=$CHECKERFRAMEWORK/checker/bin:${PATH}
 alias javacheck='java -jar "$CHECKERFRAMEWORK/checker/dist/checker.jar"'
