657288e7d424c279c6601fe88cf44054045ce289
Shubham Raj <31668258+the1derer@users.noreply.github.com>
2022-02-12 20:33:21 +0530

    [zsh] remove nvm node manager [shell] resolve issue with nvim-as-man-reader

--- shell/common.sh
+++ shell/common.sh
@@ -1,5 +1,6 @@
 # use nvim as man pager
-export MANPAGER="nvim -c 'set ft=man' -"
+export MANPAGER='nvim +Man!'
+# export MANWIDTH=999
 
 # update
 ## alias update='sudo apt update && sudo apt upgrade -y && flatpak update && flatpak uninstall --unused && nvm install node && sdk upgrade && rustup update'
