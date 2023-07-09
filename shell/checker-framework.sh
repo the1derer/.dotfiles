f2fd9552ba7f5060808277894e0aa08a0758aee4
Shubham Raj <31668258+the1derer@users.noreply.github.com>
2023-07-09 16:38:47 +0530

    correct path to use 'Study' instead of 'Work'
    Signed-off-by: Shubham Raj <31668258+the1derer@users.noreply.github.com>

--- shell/checker-framework.sh
+++ shell/checker-framework.sh
@@ -1,6 +1,6 @@
 # Alias for Checker Framework
-export CHECKERFRAMEWORK=$HOME/Work/open_source/checker_framework/build/prebuilt/ # Use PreBuilt version of Checker Framework
-# export CHECKERFRAMEWORK=$HOME/Work/open_source/checker_framework/build/typetools/checker-framework # Use Typetoolsversion of Checker Framework
-# export CHECKERFRAMEWORK=$HOME/Work/open_source/Checker_Framework/build/the1derer/checker-framework # Use the1dererversion of Checker Framework
+export CHECKERFRAMEWORK=$HOME/Study/open_source/checker_framework/build/prebuilt/ # Use PreBuilt version of Checker Framework
+# export CHECKERFRAMEWORK=$HOME/Study/open_source/checker_framework/build/typetools/checker-framework # Use Typetoolsversion of Checker Framework
+# export CHECKERFRAMEWORK=$HOME/Study/open_source/Checker_Framework/build/the1derer/checker-framework # Use the1dererversion of Checker Framework
 # export PATH=$CHECKERFRAMEWORK/checker/bin:${PATH}
 alias javacheck='java -jar "$CHECKERFRAMEWORK/checker/dist/checker.jar"'
